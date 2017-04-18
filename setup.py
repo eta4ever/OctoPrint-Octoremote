418c386ca5107415c168538ac9c68fa2446a2334
eta4ever <arlorlo@ya.ru>
2017-04-19 00:39:47 +0300

    error in name

--- setup.py
+++ setup.py
@@ -4,17 +4,17 @@
 ### Do not forget to adjust the following variables to your own plugin.
 
 # The plugin's identifier, has to be unique
-plugin_identifier = "OctoRemote-e"
+plugin_identifier = "OctoRemoteE"
 
 # The plugin's python package, should be "octoprint_<plugin identifier>", has to be unique
-plugin_package = "octoprint_OctoRemote-e"
+plugin_package = "octoprint_OctoRemoteE"
 
 # The plugin's human readable name. Can be overwritten within OctoPrint's internal data via __plugin_name__ in the
 # plugin module
-plugin_name = "OctoPrint-Octoremote-e"
+plugin_name = "OctoPrint-OctoremoteE"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
-plugin_version = "0.1"
+plugin_version = "0.0.1"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
